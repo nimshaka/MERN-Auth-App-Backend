26862c5f369effebab3f87d3c5a73551c7588c58
nimshaka <nimshakaravintha@gmail.com>
2025-02-20 14:19:35 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+
+
+mongoose
+  .connect(
+    "mongodb+srv://nimshakaravintha:A2i1BBBbZSt48ooy@mern-auth-app.obtvk.mongodb.net/?retryWrites=true&w=majority&appName=MERN-auth-app"
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
