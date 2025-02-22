c3313abc88fdb12d812431807bcc02320116faaf
nimshaka <nimshakaravintha@gmail.com>
2025-02-22 14:18:06 +0530

    add midleware

--- controllers/auth.control.js
+++ controllers/auth.control.js
@@ -1,7 +1,7 @@
 import User from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
 
-export const singup = async(req, res) => {
+export const singup = async(req, res, next) => {
    const { username, email, password } = req.body;
    const hashedPassword = bcryptjs.hashSync(password, 10);
    const newUser = new User({ username, email, password: hashedPassword }); 
@@ -9,7 +9,7 @@ export const singup = async(req, res) => {
      await newUser.save();
       res.status(200).json("User created successfully");
    } catch (err) {
-      res.status(500).json(err.message);
+      next(err);
    }
 };
 
